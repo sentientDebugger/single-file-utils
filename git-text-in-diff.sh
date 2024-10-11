2e2a20bc608a47485c271b92a7a6d3314535a30d
Carlos Enrique Sifuentes <sifuentescarlos226@gmail.com>
2024-10-11 17:45:29 -0500

    Modify git-text-in-diff to use getopts. Add optional limit to printed matching commits.

--- git-text-in-diff.sh
+++ git-text-in-diff.sh
@@ -1,32 +1,66 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# EXAMPLE USAGE:
-# path/to/git-text-in-diff.sh HEAD target-file.js searchTerm
+function uln() {
+	echo "$(tput smul)$*$(tput rmul)"
+}
 
-if [[ $# -ne 3 ]]; then
-	RED='\033[0;31m'
-	NOCOLOR='\033[0m'
-	echo -en "${RED}\
-First parameter should be the branch name or \"HEAD\".
-Second parameter should be the file to test.
-Third parameter should be the string to test for.
-${NOCOLOR}"
+function printHelp() {
+	echo "\
+Usage: git-text-in-diff -f $(uln path/to/file.md) -s $(uln searchTerm) [-b $(uln branch-name)] [-l $(uln max-commit-count)]
+Options:
+  -b $(uln branchName): The branch name to search within. Default: \"HEAD\".
+  -f $(uln fileName): The file that is to be searched.
+  -s $(uln searchTerm): The substring to be searched for in each commit's diff.
+  -l $(uln max-commit-count): The maximum number of matching commits to print out. Pass \"0\" to show all matches. Default: 0.
+"
+}
+
+branchName=HEAD
+maxCommitCount=0
+
+while getopts ":b:f:s:l:" o; do
+	case "$o" in
+		b)
+			branchName=$OPTARG
+			;;
+		f)
+			fileName=$OPTARG
+			;;
+		s)
+			searchSubject=$OPTARG
+			;;
+		l)
+			maxCommitCount=$OPTARG
+			;;
+		*)
+			printHelp
+			exit 0
+			;;
+	esac
+done
+shift $((OPTIND-1))
+
+if [[ -z ${fileName:-} || -z ${searchSubject:-} || -z ${branchName:-} || -z ${maxCommitCount:-} ]]; then
+	printHelp 1>&2
 	exit 1
 fi
-# branchName is typically just "HEAD"
-branchName=$1
-fileName=$2
-searchSubject=$3
+if [[ ! ( -r $fileName ) ]]; then
+	echo "$(tput setaf 1)$(uln $fileName) does not exist or is not readable$(tput sgr0)" 1>&2
+	exit 1
+fi
 
+(( printedCommitCount = 0 )) || true # https://www.gnu.org/software/bash/manual/html_node/Bash-Builtins.html#index-let and `set -e`
+
+# cd $(dirname fileName)
 for ref in $(git rev-list "$branchName" "$fileName")
 do
-	# if [[ $(git show -p $ref | grep $searchSubject) ]]; then
-	if [[ $(git diff-tree -p $ref "$fileName" | grep -E -e '^(\+|\-)' | grep "$searchSubject") ]]; then
-		# echo "diff for: $ref"
-		# echo
-		## git diff $ref^ $ref $fileName
-		# git diff-tree -p $ref "$fileName"
+	# The `|| [[ $? == 141 ]]` below is only needed because of `set -o pipefail` and `grep -q` causing SIGPIPE to the previous `grep`
+	# 141 - 128 == 13 == SIGPIPE
+	if git diff-tree -p $ref "$fileName" | grep -E -e '^(\+|\-)' | grep -Fq "$searchSubject" || [[ $? == 141 ]]; then
+
 		git log -n 1 $ref
+
+		(( maxCommitCount != 0 && ++printedCommitCount >= maxCommitCount )) && break
 	fi
 done
